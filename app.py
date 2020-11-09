630fcc7a544bfa73ef84bcb213647b463bd963a0
Innocent Ingabire <inno100os@gmail.com>
2020-11-09 05:37:48 +0200

    changes

--- app.py
+++ app.py
@@ -101,43 +101,61 @@ import numpy as np
 # kmeans=pickle.load(open('unsupervisedmodels.pkl','rb')) 
 
 
-def predict_kmeans(CountryName,StringencyLegacyIndexForDisplay,StringencyIndexForDisplay,	StringencyIndex,StringencyLegacyIndex,ContainmentHealthIndexForDisplay,ContainmentHealthIndex,GovernmentResponseIndexForDisplay,ConfirmedCases,ConfirmedDeaths,EconomicSupportIndexForDisplay,E2_Debtcontractrelief,EconomicSupportIndex,C3_Cancelpublicevents,C1_Schoolclosing):
-    input=np.array([[CountryName,StringencyLegacyIndexForDisplay,StringencyIndexForDisplay,	StringencyIndex,StringencyLegacyIndex,ContainmentHealthIndexForDisplay,ContainmentHealthIndex,GovernmentResponseIndexForDisplay,ConfirmedCases,ConfirmedDeaths,EconomicSupportIndexForDisplay,E2_Debtcontractrelief,EconomicSupportIndex,C3_Cancelpublicevents,C1_Schoolclosing]]).astype(np.float64)
+def predict_kmeans(CountryName, Date, C1_School closing, C1_Flag,
+       C2_Workplace closing, C2_Flag, C3_Cancel public events,
+       C4_Restrictions on gatherings, C4_Flag, C5_Close public transport,
+       C5_Flag, C6_Stay at home requirements, C6_Flag,
+       C7_Restrictions on internal movement, C7_Flag,
+       C8_International travel controls, E1_Income support, E1_Flag,
+       E2_Debt/contract relief, E3_Fiscal measures,
+       E4_International support, H1_Public information campaigns,
+       H1_Flag, H2_Testing policy, H3_Contact tracing,
+       H5_Investment in vaccines, ConfirmedCases, StringencyIndex):
+    input=np.array([[CountryName, Date, C1_School closing, C1_Flag,
+       C2_Workplace closing, C2_Flag, C3_Cancel public events,
+       C4_Restrictions on gatherings, C4_Flag, C5_Close public transport,
+       C5_Flag, C6_Stay at home requirements, C6_Flag,
+       C7_Restrictions on internal movement, C7_Flag,
+       C8_International travel controls, E1_Income support, E1_Flag,
+       E2_Debt/contract relief, E3_Fiscal measures,
+       E4_International support, H1_Public information campaigns,
+       H1_Flag, H2_Testing policy, H3_Contact tracing,
+       H5_Investment in vaccines, ConfirmedCases, StringencyIndex]]).astype(np.float64)
     prediction=kmeans.predict(input)
     return prediction
 
 def main():
-    st.title("Records of countries classified in the clusters, this is an Machine Learning assignment3")
+    st.title("Classification of Countries vs Covid")
     html_temp = """
-    <div style="background-color:blue ;padding:10px">
-    <h2 style="color:white;text-align:center;">Unsupervised Machine Learning App </h2>
+    <div style="background-color:#996515 ;padding:10px">
+    <h2 style="color:white;text-align:center;">Unsupervised KMeans Algorithm </h2>
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
     CountryName = st.text_input("CountryName",key='0')
-    StringencyLegacyIndexForDisplay = st.text_input("StringencyLegacyIndexForDisplay",key='1')
-    StringencyIndexForDisplay = st.text_input("StringencyIndexForDisplay","Type Here",key='2')
-    StringencyIndex = st.text_input("StringencyIndex","Type Here",key='3')
-    StringencyLegacyIndex = st.text_input("StringencyLegacyIndex","Type Here",key='4')
-    ContainmentHealthIndexForDisplay = st.text_input("ContainmentHealthIndexForDisplay","Type Here",key='5')
-    GovernmentResponseIndexForDisplay = st.text_input("GovernmentResponseIndexForDisplay","Type Here",key='6')
-    ContainmentHealthIndex = st.text_input("ContainmentHealthIndex","Type Here",key='7')
-    ConfirmedCases = st.text_input("ConfirmedCases","Type Here",key='8')
-    ConfirmedDeaths = st.text_input("ConfirmedDeaths","Type Here",key='9')
-    EconomicSupportIndexForDisplay = st.text_input("EconomicSupportIndexForDisplay","Type Here",key='9')
-    E2_Debtcontractrelief = st.text_input("E2_Debtcontractrelief","Type Here",key='10')
-    EconomicSupportIndex = st.text_input("EconomicSupportIndex","Type Here",key='11')
-    C3_Cancelpublicevents = st.text_input("C3_Cancelpublicevents","Type Here",key='12')
+    Date = st.text_input("StringencyLegacyIndexForDisplay",key='1')
+    C1_School closing = st.text_input("StringencyIndexForDisplay","Type Here",key='2')
+    C1_Flag = st.text_input("StringencyIndex",,key='3')
+    C2_Workplace closing = st.text_input("StringencyLegacyIndex",,key='4')
+    C2_Flag = st.text_input("C2_Flag",key='5')
+     C7_Restrictions on internal movement = st.text_input(" C7_Restrictions on internal movement",key='6')
+    C7_Flag = st.text_input("C7_Flag",key='7')
+    C8_International travel controls = st.text_input("C8_International travel controls",key='8')
+    E1_Income support = st.text_input("E1_Income support",key='9')
+    E1_Flag = st.text_input("E1_Flag",key='9')
+    E2_Debtcontractrelief = st.text_input("E2_Debtcontractrelief",key='10')
+    EconomicSupportIndex = st.text_input("EconomicSupportIndex",key='11')
+    C3_Cancelpublicevents = st.text_input("C3_Cancelpublicevents",key='12')
     C1_Schoolclosing = st.text_input("C1_Schoolclosing","Type Here",key='13')
 
     safe_html="""  
       <div style="background-color:green;padding:10px >
-       <h2 style="color:white;text-align:center;">Very safe please</h2>
+       <h2 style="color:white;text-align:center;"></h2>
        </div>
     """
     danger_html="""  
-      <div style="background-color:yellow;padding:10px >
-       <h2 style="color:red ;text-align:center;"> Hook, very danger please</h2>
+      <div style="background-color:green;padding:10px >
+       <h2 style="color:red ;text-align:center;"></h2>
        </div>
     """
 
